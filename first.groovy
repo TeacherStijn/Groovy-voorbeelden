c358d04ab0452885f06f582286700250b428e3c6
Stijn Janssen <stinow@gmail.com>
2022-01-04 12:29:52 +0100

    Update met instanceof

--- first.groovy
+++ first.groovy
@@ -12,14 +12,21 @@ println intro + titel
 
 // Variabele maken en tonen in tekst, moet met dubbele quotes
 titel = 'Qwixx'
-intro = "Dat is toch een leuk spel zeg dat $titel"
+intro = "Dat is toch een leuk spel zeg dat ${titel}"
 println intro
 
 
 // Variabele maken mag ook met def. Handiger bij gebruik meerdere threads (CPU cores):
 def titel = 'Qwixx'
-def intro = "Dat is toch een leuk spel zeg dat $titel"
+def intro = "Dat is toch een leuk spel zeg dat ${titel}"
 println intro
 
 
-// Je ziet ook: geen meldingen bij opnieuw declareren variabelen.
+// Je ziet ook: geen meldingen bij opnieuw declareren variabelen
+titel = 'Qwixx'
+println titel instanceof String
+titel = 3; // mag
+println titel instanceof Integer // (check op 'int' kan niet)
+titel = {} // mag niet
+println titel instanceof Object
+println titel !instanceof Object // nieuwe feature sinds Groovy 3
