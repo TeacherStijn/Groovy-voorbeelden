d7191ed5d06539f05a3f36a2c68c3d4c1e6d0dac
Stijn Janssen <stinow@gmail.com>
2022-05-31 14:00:46 +0200

    Update HTTP_call.groovy

--- HTTP_call.groovy
+++ HTTP_call.groovy
@@ -1,7 +1,100 @@
-def connectie = new URL('https://www.stijnjanssen.nl/api/assortiment.json').openConnection();
-def code = connectie.getResponseCode();
-println code
-if (code.equals(200)) {
-	println connectie.getInputStream();
-	println connectie.getInputStream().getText();
+{
+    "type": "array",
+    "items": {
+        "type": "object",
+        "properties": {
+            "name": {
+                "type": ["number","string"]
+            },
+            "alternate_names": {
+                "type": "array"
+            },
+            "species": {
+                "type": ["number","string"]
+            },
+            "gender": {
+                "type": ["number","string"]
+            },
+            "house": {
+                "type": ["number","string"]
+            },
+            "dateOfBirth": {
+                "type": ["number","string"]
+            },
+            "yearOfBirth": {
+                "type": [
+                    "number",
+                    "string"
+                ]
+            },
+            "wizard": {
+                "type": "boolean"
+            },
+            "ancestry": {
+                "type": ["number","string"]
+            },
+            "eyeColour": {
+                "type": ["number","string"]
+            },
+            "hairColour": {
+                "type": ["number","string"]
+            },
+            "wand": {
+                "type": "object",
+                "properties": {
+                    "wood": {
+                        "type": ["number","string"]
+                    },
+                    "core": {
+                        "type": ["number","string"]
+                    },
+                    "length": {
+                        "type": ["number","string"]
+                    }
+                }
+            },
+            "patronus": {
+                "type": ["number","string"]
+            },
+            "hogwartsStudent": {
+                "type": "boolean"
+            },
+            "hogwartsStaff": {
+                "type": "boolean"
+            },
+            "actor": {
+                "type": ["number","string"]
+            },
+            "alternate_actors": {
+                "type": "array"
+            },
+            "alive": {
+                "type": "boolean"
+            },
+            "image": {
+                "type": ["number","string"]
+            }
+        },
+        "required": [
+            "name",
+            "alternate_names",
+            "species",
+            "gender",
+            "house",
+            "dateOfBirth",
+            "yearOfBirth",
+            "wizard",
+            "ancestry",
+            "eyeColour",
+            "hairColour",
+            "wand",
+            "patronus",
+            "hogwartsStudent",
+            "hogwartsStaff",
+            "actor",
+            "alternate_actors",
+            "alive",
+            "image"
+        ]
+    }
 }
